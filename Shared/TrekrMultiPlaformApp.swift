9cefff239d0c3e6eb4d865be95d4520db90ba78b
Emily Liang <eliang@lcs.on.ca>
2022-01-18 09:27:42 -0500

    Initial Commit

---
+++ Shared/TrekrMultiPlaformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiPlaformApp.swift
+//  Shared
+//
+//  Created by Xiaoyu Liang on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiPlaformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
