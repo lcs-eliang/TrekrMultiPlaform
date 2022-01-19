3c620205f312daf4e43b2dd914f713219cec7fdf
Emily Liang <eliang@lcs.on.ca>
2022-01-19 10:10:26 -0500

    Laid out the detail view for a location.

--- Shared/TrekrMultiPlaformApp.swift
+++ Shared/TrekrMultiPlaformApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct TrekrMultiPlaformApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                LocationDetail(location:Location.example)
+            }
         }
     }
 }
