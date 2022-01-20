202b6492340c32fb389820e75f722cbf3542486f
Emily Liang <eliang@lcs.on.ca>
2022-01-20 08:27:15 -0500

    Organized the codes in the MVVM design pattern.

--- Shared/Views/TipsList.swift
+++ Shared/Views/TipsList.swift
@@ -8,43 +8,39 @@
 import SwiftUI
 
 struct TipsList: View {
-    
-    // This view creates has it's own source of truth
-    let tips: [Tip]
-    
+
+    // TipStore is a reference type
+    // It's created by this view
+    // It's a source of truth, not a derived value
+    // So, use @StateObject
+    // Establishes an initial connection to the view model for tips
+    @StateObject var dataStore = TipStore()
+
     var body: some View {
-       
-        List(tips, id: \.text, children: \.children) { tip in
+        
+        // Note how the List view in SwiftUI does many things
+        // Not just for navigation
+        // children argument must match up with the Tip type property name (children)
+        // Does NOT have to match the parameter name
+        List(dataStore.tips, id: \.text, children: \.children) { tip in
             
-            // If there are children on this particular tip...
             if tip.children != nil {
-                Label(tip.text, systemImage:"Quote.bubble")
+                Label(tip.text, systemImage: "quote.bubble")
+                    .font(.headline)
             } else {
-            Text(tip.text)
+                Text(tip.text)
             }
-            
             
         }
         .navigationTitle("Tips")
     }
-    
-    // Initializer pulls tips data from JSON
-    // Populates the array
-    init() {
         
-        // Get a pointer to the file
-        let url = Bundle.main.url(forResource: "tips", withExtension: "json")!
-        
-        // Load the contents of the JSON file
-        let data = try! Data(contentsOf: url)
-        
-        // Convert the data from the JSON file into the array
-        tips = try! JSONDecoder().decode([Tip].self, from: data)
-    }
 }
 
 struct TipsList_Previews: PreviewProvider {
     static var previews: some View {
-        TipsList()
+        NavigationView {
+            TipsList()
+        }
     }
 }
